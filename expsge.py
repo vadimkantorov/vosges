ae971ae6251405c7e671f96740104055943b5c5b
Vadim Kantorov <vadimkantorov@gmail.com>
2016-05-03 21:01:23 +0200

    Disabling cache for the report

--- expsge.py
+++ expsge.py
@@ -149,7 +149,8 @@ def html(e):
 <html>
 	<head>
 		<title>Report on %s</title>
-		<meta charset="utf-8">
+		<meta charset="utf-8" />
+		<meta http-equiv="cache-control" content="no-cache" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 		<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
